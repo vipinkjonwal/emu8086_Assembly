5721c2bbec8e93758746691c25bb245fe6ad2c65
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-16 21:48:39 +0530

    Update linear.asm

--- linear.asm
+++ linear.asm
@@ -3,9 +3,9 @@
 ;AUTHOR : VIPIN KUMAR (44)
 
 DATA SEGMENT
-    ARRAY DB 12H,04H,19H,17H,07H    ;INDEX starts at 0            
-    ELE DB 04H
-    INDEX DW ?
+    ARRAY DB 12H,04H,19H,17H,07H            ;INDEX starts at 0, Initializing of array ARRAY            
+    ELE DB 04H                              ;ELE Variable, element need to be searched in ARRAY
+    INDEX DW ?                              ;Memory Allocation for INDEX of ELE found.
 DATA ENDS
 
 CODE SEGMENT
@@ -15,26 +15,25 @@ CODE SEGMENT
         MOV AX,DATA
         MOV DS,AX
         
-        MOV AX,0
+        XOR AX,AX                           ;Setting AX = 0 and clearing all flags of PSW    
         MOV SI,0
-        MOV CX,5
-        MOV BX,OFFSET ARRAY
+        MOV CX,5                            ;Initializing counter value to 5
+        MOV BX,OFFSET ARRAY                 ;Setting of address of ARRAY to register BX
         
     SEARCH:
         MOV DL,BX[SI]
         CMP DL,ELE
-        JZ FOUND
+        JZ FOUND                            ;If ELE found, jump to label FOUND
         INC SI
         LOOP SEARCH
 
     NOTFOUND:
-        MOV INDEX,-1                ;INDEX=-1 implies ELEMENT NOT FOUND.
+        MOV INDEX,-1                        ;INDEX=-1 implies ELEMENT NOT FOUND.
         JMP EXIT
         
     FOUND:
-        MOV INDEX,SI                ;INDEX=SI implies ELEMENT FOUND AT INDEDX SI
-        JMP EXIT
-        
+        MOV INDEX,SI                        ;INDEX = SI implies ELEMENT FOUND AT INDEDX SI
+                
 EXIT:
 CODE ENDS
 END START
